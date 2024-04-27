1448e4df34b32c0feff4df509981c184f47ac120
tslee <tslee.dev@gmail.com>
2024-04-28 00:22:12 +0900

    Cocoapods 제거, SPM으로만 제공되게 수정

--- Example/Pods/Target Support Files/TSWebView/TSWebView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TSWebView : NSObject
-@end
-@implementation PodsDummy_TSWebView
-@end
---
+++ Example/TSWebViewDemo/TSWebViewDemo/TSWebViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TSWebViewDemo
+//
+//  Created by TAE SU LEE on 4/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
